905ee0c4e47bfd612a912c5977a737a0d24d11a7
sophiex59 <118982863+sophiex59@users.noreply.github.com>
2023-04-04 12:32:33 +0100

    Update streamlit_app.py
    added breakfast menu header and 3 brekkie items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.title("My Parents Healthy DIner")
+
+streamlit.header('Break-Your-Fast Menu')
+streamlit.text('Omega 3')
+streamlit.text('Kale')
+streamlit.text('Hard-Boiled')
