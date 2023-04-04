9adeb2d74371148a324420f544bea87906169afc
sophiex59 <118982863+sophiex59@users.noreply.github.com>
2023-04-04 12:54:09 +0100

    Update streamlit_app.py
    display as json, then normalise it and put it ina table

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
+
+# normalises the json response
+fruityvicce_normalized = pandas.json_normalize(fruityvice_response.json())
+# outputs it as table
+streamlit.dataframe(fruityvice_normalized)
