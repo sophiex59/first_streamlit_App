88c5fc71fd48bf32f53712eaf0a93afe14cf2e15
sophiex59 <118982863+sophiex59@users.noreply.github.com>
2023-04-04 12:34:52 +0100

    Update streamlit_app.py
    added second header and emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit as st
 st.title("My Parents Healthy DIner")
 
 streamlit.header('Break-Your-Fast Menu')
-streamlit.text('Omega 3')
-streamlit.text('Kale')
-streamlit.text('Hard-Boiled')
+streamlit.text('Omega 🥣 3')
+streamlit.text('Kal🥗e')
+streamlit.text('Hard-Boiled🐔')
+streamlit.text('Avo🥑🍞 Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
