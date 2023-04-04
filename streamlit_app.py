7a7b6878b535f6298be5d13236e78d6558e4e055
sophiex59 <118982863+sophiex59@users.noreply.github.com>
2023-04-04 14:22:54 +0100

    Update streamlit_app.py
    changing url

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What Fruil would you like info on?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
 # normalises the json response
